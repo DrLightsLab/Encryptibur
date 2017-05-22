5d855cd908baccc3c6087b92f2954ac49e627362
Doc Light <doctorlightslab@gmail.com>
2017-05-21 22:01:19 -0400

    This creates the Encryptibur and Category class inside of the models.py file.

--- Encryptibur/classes/user.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
-    encryptibur_id = models.ForeignKey(Encryptibur, on_delete=models.CASCADE)
--- Encryptibur/models.py
+++ Encryptibur/models.py
@@ -1,3 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Encryptibur(models.Model):
+    user = model.ForeignKey(User, on_delete=models.CASCADE)
+    category = model.CharField(max_length=30)
+    account_name = model.CharField(max_length=30)
+    encrypted_value = model.CharField(max_length=50)
+    encrypted_length = model.CharField(max_length=10)
